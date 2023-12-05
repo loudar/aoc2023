9136737886e5618d33a1350901ed230c383fb559
Alexander Fritsch <35202909+loudar@users.noreply.github.com>
2023-12-05 13:39:39 +0100

    day 3 part 1 test works

---
+++ 3.mjs
@@ -0,0 +1,75 @@
+import fs from "fs";
+
+export class Day3 {
+    static run(input) {
+        const rows = this.getRows(input);
+        let matchedNumbers = [];
+        for (let i = 0; i < rows.length; i++) {
+            let adjacentRows = [rows[i]];
+            if (i > 0) {
+                adjacentRows.push(rows[i - 1]);
+            }
+            if (i < rows.length - 1) {
+                adjacentRows.push(rows[i + 1]);
+            }
+            for (let adjacentRow of adjacentRows) {
+                for (let n of rows[i].numbers) {
+                    for (let s of adjacentRow.symbols) {
+                        if (s.index >= n.startIndex - 1 && s.index <= n.endIndex + 1) {
+                            matchedNumbers.push(n.number);
+                        }
+                    }
+                }
+            }
+        }
+        const result = matchedNumbers.reduce((a, n) => a + n, 0);
+        console.log(`Result is ${result}`);
+    }
+
+    static getRows(input) {
+        const lines = input.split("\n");
+        const symbolsRegex = new RegExp(/[^.1-9]/gd);
+        const rows = [];
+        for (const line of lines) {
+            let numbers = line.split(/[^1-9]/);
+            numbers = numbers
+                .filter(n => n !== '')
+                .map(n => {
+                    const startIndex = line.indexOf(n);
+                    return {
+                        number: parseInt(n),
+                        startIndex,
+                        endIndex: startIndex + n.length - 1
+                    };
+                });
+            const matchedSymbols = [...line.matchAll(symbolsRegex)];
+            let symbols = [];
+            if (matchedSymbols) {
+                let i = -1;
+                symbols = matchedSymbols.map(s => {
+                    i++;
+                    return {
+                        symbol: s.toString(),
+                        index: s.index,
+                    };
+                });
+            }
+            rows.push({numbers, symbols});
+        }
+        return rows;
+    }
+}
+
+const fileContent = fs.readFileSync(`inputs/3.txt`);
+const input = fileContent.toString();
+
+Day3.run(`467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`);
