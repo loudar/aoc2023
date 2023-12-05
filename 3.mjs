ba4014662ca3ee4f4302bcf4d167f366d17d755a
Alexander Fritsch <35202909+loudar@users.noreply.github.com>
2023-12-05 14:20:44 +0100

    day 3 part 2

--- 3.mjs
+++ 3.mjs
@@ -1,7 +1,7 @@
 import fs from "fs";
 
 export class Day3 {
-    static run(input) {
+    static runPart1(input) {
         const rows = this.getRows(input);
         let matchedNumbers = [];
         for (let i = 0; i < rows.length; i++) {
@@ -26,12 +26,46 @@ export class Day3 {
         console.log(`Result is ${result}`);
     }
 
+    static runPart2(input) {
+        const rows = this.getRows(input);
+        let multiplicationResults = [];
+        for (let i = 0; i < rows.length; i++) {
+            let adjacentRows = [rows[i]];
+            if (i > 0) {
+                adjacentRows.push(rows[i - 1]);
+            }
+            if (i < rows.length - 1) {
+                adjacentRows.push(rows[i + 1]);
+            }
+            for (let s of rows[i].symbols) {
+                let matchingNumbers = [];
+                if (s.symbol !== '*') {
+                    continue;
+                }
+                for (let adjacentRow of adjacentRows) {
+                    for (let n of adjacentRow.numbers) {
+                        if (s.index >= n.startIndex - 1 && s.index <= n.endIndex + 1) {
+                            matchingNumbers.push(n.number);
+                        }
+                    }
+                }
+                if (matchingNumbers.length > 1) {
+                    const partResult = matchingNumbers.reduce((a, n) => a * n, 1);
+                    multiplicationResults.push(partResult);
+                }
+            }
+        }
+        const result = multiplicationResults.reduce((a, n) => a + n, 0);
+        console.log(`Result is ${result}`);
+    }
+
     static getRows(input) {
         const lines = input.split("\n");
         const symbolsRegex = new RegExp(/[^.0-9]/gd);
         const numbersRegex = new RegExp(/[0-9]+/gd);
         const rows = [];
-        for (const line of lines) {
+        for (let line of lines) {
+            line = line.trim();
             let numbers = [...line.matchAll(numbersRegex)];
             numbers = numbers
                 .map(n => {
@@ -63,7 +97,7 @@ export class Day3 {
 const fileContent = fs.readFileSync(`inputs/3.txt`);
 const input = fileContent.toString();
 
-Day3.run(`467..114..
+Day3.runPart1(`467..114..
 ...*......
 ..35..633.
 ......#...
@@ -73,4 +107,15 @@ Day3.run(`467..114..
 ......755.
 ...$.*....
 .664.598..`);
-Day3.run(input);
+Day3.runPart1(input);
+Day3.runPart2(`467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`);
+Day3.runPart2(input);
